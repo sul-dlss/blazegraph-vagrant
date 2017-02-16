b3434240628ca10ce4b987fc1ad943ea52214b2f
Darren Weber <dweber.consulting@gmail.com>
2017-02-16 15:36:04 -0800

    Blazegraph installation can build and use a debian package

--- scripts/packages/blazegraph.sh
+++ scripts/packages/blazegraph.sh
@@ -8,17 +8,37 @@ if [ -f "$SHARED_DIR/scripts/config" ]; then
   . $SHARED_DIR/scripts/config
 fi
 
-if [ ! -f "$DOWNLOAD_DIR/$BLAZEGRAPH_VERSION/scripts/mavenInstall.sh" ]; then
+if [ ! -d /etc/blazegraph ]; then
   echo -n "Downloading Blazegraph..."
   cd $DOWNLOAD_DIR
   git clone -b $BLAZEGRAPH_VERSION --single-branch https://github.com/blazegraph/database.git $BLAZEGRAPH_VERSION
   chown -hR vagrant:vagrant $BLAZEGRAPH_VERSION
-  cd $BLAZEGRAPH_VERSION
-  ./scripts/mavenInstall.sh
+  cd $BLAZEGRAPH_VERSION/blazegraph-deb/
+  mvn clean package
+  sudo apt-get update
+  sudo apt-get install -y python-software-properties
+  sudo dpkg --install ./target/blazegraph-deb-2.1.4.deb 
+
+  # The installer echoes the following:
+  #
+  # ***********************************************************
+  # * The following kernel tuning parameters are recommended. *
+  # * They are not automatically configured.                  *
+  # ***********************************************************
+  # 
+  # echo 1 > /proc/sys/net/ipv4/tcp_tw_reuse
+  # echo 'vm.swappiness = 0' >> /etc/sysctl.d/10-vm.swappiness.conf
+  # sysctl -p || true >& /dev/null
+
+  # When trying to follow these instructions, the system does not allow modification to these files.
+  #
+  #sudo echo 1 > /proc/sys/net/ipv4/tcp_tw_reuse
+  #sudo echo 'vm.swappiness = 0' >> /etc/sysctl.d/10-vm.swappiness.conf
+  #sudo sysctl -p || true >& /dev/null
+
+  sudo apt-get install -f -y
+  sudo service blazegraph restart
+
   echo " done"
 fi
-
-echo "Starting Blazegraph"
-cd $DOWNLOAD_DIR/$BLAZEGRAPH_VERSION
-./scripts/startBlazegraph.sh &
 
